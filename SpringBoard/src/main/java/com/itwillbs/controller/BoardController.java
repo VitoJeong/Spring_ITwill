b60061a81561109c0bbf13b29a035cd2dbfb3121
VitoJeong <ckdghk987@gmail.com>
2020-08-28 14:10:32 +0900

    0828_1

--- SpringBoard/src/main/java/com/itwillbs/controller/BoardController.java
+++ SpringBoard/src/main/java/com/itwillbs/controller/BoardController.java
@@ -5,10 +5,23 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.itwillbs.domain.BoardVO;
 import com.itwillbs.service.BoardService;
 
+// @RequestMapping("/board/*")
+// board로 시작되는 모든 주소에 대한 처리
+// -> 컨트롤러 클래스에  사용되는 RequestMapping의미는 컨트롤러 구분목적
+
+
 @Controller
+@RequestMapping("/board/*")
 public class BoardController {
 	// 서비스 호출 -> DAO
 	
@@ -17,6 +30,90 @@ public class BoardController {
 	private BoardService service;
 	
 	// 로그 객체
-	private static Logger logger = 
+	private static final Logger logger = 
 			LoggerFactory.getLogger(BoardController.class);
+	
+	// 글쓰기 동작 - 입력 -> view 페이지 이동
+	// method = {RequestMethod.GET, RequestMethod.POST}
+	// - > 두 가지 방식 모두 처리 가능
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public void registerGET() throws Exception{
+		logger.info(" /register 호출 -> register.jsp로 이동");
+		logger.info("submit() 사용해서 정보를 전달 ");
+		
+	}
+	
+	
+	// 글쓰기 동작 - 처리
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String registerPOST(BoardVO vo, Model model, RedirectAttributes rttr) throws Exception{
+		// 메서드 전달인자는 상황에 따라서 get()/set() 역할을 처리
+		// jsp -> 컨트롤러(정보전달) 스프링이 해당객체를 생성후 정보를 자동 전달
+		
+		// Model 객체는 스프링에서 제공하는 데이터 전달 객체
+		// (키, 값) 쌍으로 데이터 저장 -> 전달받는 jsp페이지에서 el표현식으로 사용가능
+		
+		logger.info("/register post 요청");
+		logger.info("전달 데이터 : "+vo);
+		
+		// DB 글쓰기 동작(서비스를 통해서 DAO이동)
+		// DI 주입된 서비스 객체를 사용해서 글쓰기 동작
+		service.regist(vo);
+		
+		logger.info("서비스 처리 완료(글 등록완료)");
+		
+		// model 객체에 정보를 저장해서 페이지 이동
+		//model.addAttribute("result", "SUCCESS!");
+		// get 방식(redirect) : 해당 attribute 정보를 주소(URL)에 표시
+		// post 방식 : 정보를 Body에 저장
+		
+		// RedirectAttribute 객체 사용 페이지이동
+		rttr.addFlashAttribute("result", "SUCCESS@");
+		
+		/*
+		 * 
+		 * 데이터를 저장해서 View 이동객체
+		 * 
+		 * Model 객체 - addAtribute()
+		 * - (key, value)쌍으로 데이터 저장
+		 * - get : 주소에 보임, post : 주소에 안보임
+		 * - F5(새로고침) : 정보 유지
+		 * 
+		 * RedirectAttributes 객체 - addFlashAttribute()
+		 * - (key, value)쌍으로 데이터 저장
+		 * - get : 주소에 안보임, post : 주소에 안보임
+		 * - F5(새로고침) : 정보 1회만 사용후 사라짐
+		 * 
+		*/
+		
+		// return "/board/success";
+		// 페이지 이동 (글 목록페이지)
+		//return "/board/listAll";
+		// 페이지이동 (화면전환-> 다른동작)
+		return "redirect:/board/listAll";
+	}
+	
+	// 글목록 동작(/board/listAll)
+	// http://localhost:8082/board/listAll?result=SUCCESS%21
+	@RequestMapping(value = "/listAll", method = RequestMethod.GET)
+	public void listAllGET(@ModelAttribute("result") String result) throws Exception{
+		
+		// @ModelAttribute("result") String result
+		// -> model 객체에 있는 속성값을 가져오는 어노테이션
+		// 변수에 저장 후 (변수값은 컨트롤러에서 사용)
+		// 변수에 저장 후 -> 연결돼있는 view(jsp)에 전달(el 표현식 -> model 저장한 키값 호출)
+		
+		if(result.equals("SUCCESS!")) {
+			logger.info("/register POST -> /listAll GET (리다이렉트)");
+		} else {
+			logger.info(" / listAll GET 호출");
+		}
+		
+		// DB 글 목록을 가져와서 -> view(jsp) 전달 출력
+		
+	}
+	
 }
+
+
+
---
+++ SpringBoard/src/main/webapp/WEB-INF/views/board/listAll.jsp
@@ -0,0 +1,77 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page session="false"%>
+
+<%@ include file="../include/header.jsp" %>
+
+
+<!-- Main content -->
+<section class="content">
+	<div class="row">
+		<!-- left column -->
+		<div class="col-md-12">
+			<!-- general form elements -->
+
+			<div class="box">
+				<div class="box-header with-border">
+					<h3 class="box-title">글 목록:[${result }]</h3>
+				</div>
+				<div class="box-body">
+				
+				<table class="table table-bordered">
+					<tr>
+						<th style="width: 60px">글번호</th>
+						<th>제목</th>
+						<th>글쓴이</th>
+						<th>날짜</th>
+						<th style="width: 60px">조회수</th>
+					</tr>
+
+
+				</table>
+
+				</div>
+				<!-- /.box-body -->
+				<div class="box-footer">Footer</div>
+				<!-- /.box-footer-->
+				<div class="box-footer">
+						<button type="button" class="btn btn-primary"
+							onclick="location.href='/board/register'">
+							글쓰기
+						</button>
+				</div>
+			</div>
+		</div>
+		<!--/.col (left) -->
+
+	</div>
+	<!-- /.row -->
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<!-- JavaScript 사용 -->
+<!-- 글쓴후 페이지 이동/페이지 이동 구문 -->
+<script type="text/javascript">
+
+	/* 전달된 속성의 값을 저장해서 비교한 후 메세지 출력 */
+	// 속성(attribute)에 저장된 정보를 JS에 전달
+	var result = '${result}';
+
+	if(result == 'SUCCESS@'){
+		// 글쓰고 리스트 페이지 왔을때만 출력
+		alert("글쓰기 완료!<br>" +result);
+		
+	}
+	
+	
+</script>
+
+
+<%@ include file="../include/footer.jsp" %>
+
+
+
+
+
